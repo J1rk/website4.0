948b284ad314319b87a62c5c2bdf24a00c21f245
Jirka Tomasek <tomasek.jirka@gmail.com>
2014-04-08 22:16:38 +0200

    Grunt connect proxy setup to rails api

--- sw-front/app/scripts/controllers/main.js
+++ sw-front/app/scripts/controllers/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('angAdminApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $resource) {
+    $scope.articles = $resource('/api/admin/articles').query();
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
