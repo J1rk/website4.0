6315de82ac54facbcdc08c01b86c9bba1bb05857
Jirka Tomasek <tomasek.jirka@gmail.com>
2014-04-08 20:31:27 +0200

    Installed karma

--- sw-front/karma.conf.js
+++ sw-front/karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration
 // http://karma-runner.github.io/0.10/config/configuration-file.html
+'use strict';
 
 module.exports = function(config) {
   config.set({
@@ -35,7 +36,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // Start these browsers, currently available:
