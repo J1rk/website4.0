b965b1f3047e8c6bcee43ffdab2eb0960e24483d
Jirka Tomasek <tomasek.jirka@gmail.com>
2014-04-09 15:34:05 +0200

    Renamed main to admin, added bootstrap_and_overrides

--- sw-front/Gruntfile.js
+++ sw-front/Gruntfile.js
@@ -377,7 +377,7 @@ module.exports = function (grunt) {
     // cssmin: {
     //   dist: {
     //     files: {
-    //       '<%= yeoman.dist %>/styles/main.css': [
+    //       '<%= yeoman.dist %>/styles/admin.css': [
     //         '.tmp/styles/{,*/}*.css',
     //         '<%= yeoman.app %>/styles/{,*/}*.css'
     //       ]
