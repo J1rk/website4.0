a7b44f4a2d7e9e6c20357f0000ed715fd55629c6
Jirka Tomasek <tomasek.jirka@gmail.com>
2014-04-09 20:17:45 +0200

    Renamed topbar_controller to navigation controller

--- sw-front/app/scripts/controllers/navigation.js
+++ sw-front/app/scripts/controllers/navigation.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('angAdminApp').controller('TopNavbarCtrl', function($scope, $location) {
+angular.module('angAdminApp').controller('NavigationCtrl', function($scope, $location) {
   $scope.isActive = function(path) {
     return path === $location.path();
   };
